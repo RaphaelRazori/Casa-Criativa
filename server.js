f0ad34f79c23a730ca1afad4e2abccfda37204df
RAPHAEL <raphaelrazori@hotmail.com>
2020-06-10 15:47:43 -0300

    Iniciando um projeto no github

---
+++ server.js
@@ -0,0 +1,115 @@
+const express = require("express")
+const server = express()
+
+const db = require("./db")
+
+//const ideas = [
+//    {//
+//        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+//        title: "Cursos de Programação",
+//        category: "Estudo",
+//        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum sunt",
+//        url:"https://rocketseat.com.br",
+//    },
+//    {
+//        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+//        title: "Exercícios",
+//        category: "Saúde",
+//        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum sunt",
+ //       url:"https://rocketseat.com.br",
+ //   },
+ //   {
+//        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+//        title: "Meditação",
+//        category: "Mentalidade",
+//        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum sunt",
+//        url:"https://rocketseat.com.br",
+//    },
+//    {
+//        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+ //       title: "Karaokê",
+//        category: "Treinamento K-Idol",
+//        description: "Treinar para se um dia receber uma proposta da JYP estar pronto para ser um idol",
+//        url:"https://rocketseat.com.br",
+//    }
+//]
+
+server.use(express.static("public"))
+
+server.use(express.urlencoded({extended: true}))
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("views", {
+    express: server,
+    noCache: true,
+})
+
+server.get("/", function(req, res){
+
+    db.all('SELECT * FROM ideas', function(err, rows){
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
+        }
+
+        const reversedIdeas = [...rows].reverse()
+
+        let lastIdeas = []
+        for(let idea of reversedIdeas) {
+            if(lastIdeas.length < 2) {
+                lastIdeas.push(idea)
+            }
+        }
+        return res.render("index.html", {ideas: lastIdeas})
+    })
+    
+    
+})
+
+server.get("/ideias", function(req, res){
+
+    db.all('SELECT * FROM ideas', function(err, rows){
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
+        }
+        
+        const reversedIdeas = [...rows].reverse()
+    
+        return res.render("ideias.html", {ideas: reversedIdeas})
+    })
+})
+
+server.post("/", function(req, res){
+    const query = `
+        INSERT INTO ideas(
+            image,
+            title,
+            category,
+            description,
+            link
+        ) VALUES (?,?,?,?,?);
+    `
+
+   const values = [
+        req.body.image,
+        req.body.title,
+        req.body.category,
+        req.body.description,
+        req.body.link,
+
+    ]
+    
+    db.run(query, values, function(err){
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
+        }
+
+        return res.redirect("/ideias")
+
+    })
+
+})
+
+server.listen(3000)
